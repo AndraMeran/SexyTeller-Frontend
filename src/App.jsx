884b2f778335d34493341132fdd58cc01313e2d7
AndraMeran <andrameran@yahoo.it>
2026-04-26 20:13:22 +0200

    Aggiunto .env, .env.example e aggiornato .gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Homepage from "./pages/Homepage"
+import Categoria from "./pages/Categoria"
+import ArticoloSingolo from "./pages/ArticoloSingolo"
+import ProfiloUtente from "./pages/ProfiloUtente"
+import Editor from "./pages/Editor"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Manifesto from "./pages/Manifesto"
+import Impostazioni from "./pages/Impostazioni"
+import Admin from "./pages/Admin"
+
 function App() {
   return (
-    <div>
-      <h1>SexyTeller</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/categoria/:nome" element={<Categoria />} />
+        <Route path="/articolo/:id" element={<ArticoloSingolo />} />
+        <Route path="/@:handle" element={<ProfiloUtente />} />
+        <Route path="/manifesto" element={<Manifesto />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/crea" element={<Editor />} />
+        <Route path="/impostazioni" element={<Impostazioni />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
